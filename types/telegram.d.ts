37de79eb8e5ff151db29ea42511d9aff2f5f67cc
vedreal <vedvlev@gmail.com>
2025-12-27 17:38:11 +0700

    Update telegram.d.ts

--- types/telegram.d.ts
+++ types/telegram.d.ts
@@ -1,6 +1,16 @@
+interface AdsgramAdController {
+  show(): Promise<{ done: boolean; description?: string; state?: string }>;
+}
+
+interface AdsgramSDK {
+  init(params: { blockId: string }): AdsgramAdController;
+}
+
 declare global {
   interface Window {
     Telegram: any;
-    Adsgram: any;
+    Adsgram?: AdsgramSDK;
   }
 }
+
+export {};
